56103d93ddd7714bf18403fbddf7ead1bba94efc
Leonardo Borges <leo.nunes.borges@hotmail.com>
2025-02-04 00:30:20 -0300

    maior O(n)

--- src/Vetores/Vetores.java
+++ src/Vetores/Vetores.java
@@ -44,21 +44,20 @@ public class Vetores {
         int dias = 10;
         double[] precos = new double[dias];
         double media= 0;
-        double aux1 = precos[0];
-        int[] aux2 = new int[dias];
+        double aux1 = Double.MIN_VALUE;;
+        int[] aux2 = new int[1];
 
         for (int i= 0; i < 10; i++){
             System.out.println("Digite o valor do imovel " + i);
             precos[i] = sc.nextDouble();
         }
 
-        for(int i = 0; i <10; i++){
-           if (precos[i] >= aux){
-               aux1 = precos[i];
-           } else {
-               aux1 = precos[dias];
-               dias--;
-           }
+        for (int i = 0; i < 10; i++) {
+            if (precos[i] > aux1) {
+                aux1 = precos[i];
+                aux2[0] = i;
+            }
         }
+        System.out.println("Maior: " + aux1);
     }
 }
