1f165d78e4521c8c48a6dcca65ad9125fe9fe07d
Leonardo Borges <leo.nunes.borges@hotmail.com>
2025-02-06 00:27:39 -0300

    exercicios

--- src/Matriz/Ex4.java
+++ src/Matriz/Ex4.java
@@ -2,15 +2,29 @@ package Matriz;
 
 public class Ex4 {
     public static void main(String[] args) {
-            int[][] valores =
-                    {{1,2,3},
-                            {4,5,6},
-                            {7,8,9}};
+        int[][] valores =
+                {{1, 2, 3},
+                        {4, 5, 6}
+                };
 
-        for (int i=0; i < ; i++){
-            for (int j = 0; j<; j++){
 
+        int linhas = valores.length;
+        int colunas = valores[0].length;
+        int[][] transposta = new int[colunas][linhas];
+
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                transposta[j][i] = valores[i][j];
             }
+        }
+
+        // Exibir a matriz transposta
+        System.out.println("Matriz Transposta:");
+        for (int i = 0; i < transposta.length; i++) {
+            for (int j = 0; j < transposta[i].length; j++) {
+                System.out.print(transposta[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
--- src/Matriz/Ex5.java
+++ src/Matriz/Ex5.java
@@ -2,13 +2,17 @@ package Matriz;
 
 public class Ex5 {
     public static void main(String[] args) {
-        int[][] valores =  new int[][];
-        valores = {{}};
+        int[][] valores =
+                {{1, 2, 3},
+                        {4, 5, 6}
+                };
 
-        for (int i=0; i < ; i++){
-            for (int j = 0; j<; j++){
 
-            }
-        }
+        int linhas = valores.length;
+        int colunas = valores[0].length;
+        int p = 4;
+        int[][] transposta = new int[colunas][p];
+
+
     }
 }
