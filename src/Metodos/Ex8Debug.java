8c8ddb7e064b3e463ddb72d80f47dea30c1ef858
Leonardo Borges <leo.nunes.borges@hotmail.com>
2025-02-14 01:49:09 -0300

    exercicio 8 nao acabado

---
+++ src/Metodos/Ex8Debug.java
@@ -0,0 +1,55 @@
+package Metodos;
+
+import java.util.Scanner;
+
+public class Ex8Debug {
+    public static void main(String[] args) {
+        double p =25000, r = 5.0/100, valorJuros = 0, amortizacao = 0;
+        int n = 10;
+        double   pmt = (p * r) / (1-(Math.pow((1+r), -n)));
+        Object[][] matriz = new Object[12][5];
+        Scanner sc = new Scanner(System.in);
+
+        /*
+        numParcelas
+        saldoDevAntes
+        valorJuros
+        amortizacao
+        saldoDevDps
+         */
+
+        for (int i = 0; i < matriz[0].length; i++) {
+            matriz[0][i] = sc.next();
+        }
+
+        for (int i = 1; i <n+1 ; i++) {
+            for (int j = 0; j < matriz[0].length ; j++) {
+                switch (j) {
+                    case (0):
+                        matriz[i][j] = n - i + 1;
+                        break;
+                    case (1):
+                        matriz[i][j] = p + amortizacao;
+                        break;
+                    case (2):
+                        matriz[i][j] = pmt * r;
+                        break;
+                    case (3):
+                        matriz[i][j] = pmt-valorJuros;
+                        break;
+                    case (4):
+                        matriz[i][j] = p - amortizacao;
+                        break;
+                }
+            }
+        }
+
+        // Exibindo a matriz
+        for (Object[] linha : matriz) {
+            for (Object elemento : linha) {
+                System.out.print((elemento != null ? elemento : "") + "\t");
+            }
+            System.out.println(); // Quebra de linha após cada linha
+        }
+    }
+}
